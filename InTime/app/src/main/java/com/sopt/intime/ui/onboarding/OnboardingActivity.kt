a2a7be78ab0ee393e9c8977312577602362fee01
s9hn <tnghqn@naver.com>
2023-11-26 02:17:08 +0900

    [feat] : 온보딩 뷰 뷰페이저 및 버튼 구현

--- InTime/app/src/main/java/com/sopt/intime/ui/onboarding/OnboardingActivity.kt
+++ InTime/app/src/main/java/com/sopt/intime/ui/onboarding/OnboardingActivity.kt
@@ -3,6 +3,7 @@ package com.sopt.intime.ui.onboarding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.intime.databinding.ActivityOnboardingBinding
+import com.sopt.intime.ui.timeSetting.TimeSettingActivity
 
 class OnboardingActivity : AppCompatActivity() {
     private val binding: ActivityOnboardingBinding by lazy {
@@ -11,7 +12,14 @@ class OnboardingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(binding.root)
+
+        binding.btnOnboardingNext.setOnClickListener {
+            navigateToTimeSettingActivity()
+        }
+    }
+
+    private fun navigateToTimeSettingActivity() {
+        startActivity(TimeSettingActivity.from(this))
     }
 }
